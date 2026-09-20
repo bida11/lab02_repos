b36852fe17587162c8d63ad922970ca341ebfb8f
bida11 <bida1104@gmail.com>
2026-09-20 18:21:00 +0300

    first commit in branch_02

---
+++ ЛР2.1/ЛР2.1.cpp
@@ -0,0 +1,34 @@
+﻿// Lab_02.cpp
+// < Біда Олександр >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double Pi = 4 * atan(1.); // число Пі
+    double alpha;            // вхідний параметр α
+    double z1;               // результат обчислення 1-го виразу
+    double z2;               // результат обчислення 2-го виразу
+
+    cout << "alpha = ";
+    cin >> alpha;
+
+    // Обчислення z1
+    z1 = 2 * pow(sin(3 * Pi - 2 * alpha), 2) * pow(cos(5 * Pi + 2 * alpha), 2);
+
+    // Обчислення z2
+    z2 = 1.0 / 4 - 1.0 / 4 * sin(5.0 / 2 * Pi - 8 * alpha);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
